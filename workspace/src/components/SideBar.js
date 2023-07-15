f0b45feb4a8daea7783093f0148eeb50916df704
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:59:22 +0700

    aider: Add onArticleSelect prop to SideBar component and call it with chapterId and articleId when an article is clicked in the sidebar.
    # Aider chat conversation:
    
    # Context:
    USER: display articlesMock content in `ContentArea` when select an article from sidebar by id

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,17 +1,19 @@
-// entire file content ...
-
 import React, { useEffect, useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import chaptersMock from '../data/ChaptersMock';
 import articlesMock from '../data/ArticlesMock'; // Add import statement for articlesMock
 import './SideBar.css'; // Add import statement for SideBar.css
 
-function SideBar() {
+function SideBar({ onArticleSelect }) { // Add onArticleSelect prop
   const [chapters, setChapters] = useState([]);
 
   useEffect(() => {
     setChapters(chaptersMock);
   }, []);
+
+  const handleArticleSelect = (chapterId, articleId) => {
+    onArticleSelect(chapterId, articleId); // Call onArticleSelect prop with chapterId and articleId
+  };
 
   return (
     <Sidebar>
@@ -19,7 +21,7 @@ function SideBar() {
         {chapters.map(chapter => (
           <SubMenu title={`${chapter.title}`} key={`chapter-${chapter.id}`} label={`Chapter ${chapter.id}: ${chapter.title}`}>
             {chapter.articles.map(article => (
-              <MenuItem key={`article-${chapter.id}-${article.id}`}>
+              <MenuItem key={`article-${chapter.id}-${article.id}`} onClick={() => handleArticleSelect(chapter.id, article.id)}>
                 {article.title}
               </MenuItem>
             ))}
