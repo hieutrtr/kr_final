442a6fe7d7a3c8252a4bfcf73097d2fcb1a2ac1f
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:18:56 +0700

    aider: Added chaptersMock to SideBar component and created a new file for chaptersMock.
    # Aider chat conversation:
    
    # Context:
    USER: ChaptersMock is define from file src/data/Mocks.js

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,28 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import chaptersMock from '../data/Mocks';
 
 function SideBar() {
   const [chapters, setChapters] = useState([]);
-
-  const chaptersMock = [
-    {
-      id: 1,
-      articles: [
-        { id: 1, title: 'Article 1' },
-        { id: 2, title: 'Article 2' },
-        { id: 3, title: 'Article 3' }
-      ]
-    },
-    {
-      id: 2,
-      articles: [
-        { id: 4, title: 'Article 4' },
-        { id: 5, title: 'Article 5' },
-        { id: 6, title: 'Article 6' }
-      ]
-    },
-    // ... add more chapters and articles as needed
-  ];
 
   useEffect(() => {
     setChapters(chaptersMock);
---
+++ workspace/src/data/Mocks.js
@@ -0,0 +1,21 @@
+const chaptersMock = [
+  {
+    id: 1,
+    articles: [
+      { id: 1, title: 'Article 1' },
+      { id: 2, title: 'Article 2' },
+      { id: 3, title: 'Article 3' }
+    ]
+  },
+  {
+    id: 2,
+    articles: [
+      { id: 4, title: 'Article 4' },
+      { id: 5, title: 'Article 5' },
+      { id: 6, title: 'Article 6' }
+    ]
+  },
+  // ... add more chapters and articles as needed
+];
+
+export default chaptersMock;
