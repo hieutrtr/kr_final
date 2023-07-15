cb114d7bb0d0589707ee5856a52b54e3605c261b
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:56:10 +0700

    aider: Display selected article content in `ContentArea` when selected from sidebar by id.
    # Aider chat conversation:
    
    # Context:
    USER: display articlesMock content in `ContentArea` when select from sidebar by id

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,17 +1,19 @@
-// entire file content ...
-
 import React, { useEffect, useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import chaptersMock from '../data/Mocks';
-import articlesMock from '../data/ArticlesMock'; // Add import statement for articlesMock
-import './SideBar.css'; // Add import statement for SideBar.css
+import articlesMock from '../data/ArticlesMock';
+import './SideBar.css';
 
-function SideBar() {
+function SideBar({ onArticleSelect }) {
   const [chapters, setChapters] = useState([]);
 
   useEffect(() => {
     setChapters(chaptersMock);
   }, []);
+
+  const handleArticleSelect = (chapterId, articleId) => {
+    onArticleSelect(chapterId, articleId);
+  };
 
   return (
     <Sidebar>
@@ -19,7 +21,7 @@ function SideBar() {
         {chapters.map(chapter => (
           <SubMenu title={`${chapter.title}`} key={`chapter-${chapter.id}`} label={`Chapter ${chapter.id}: ${chapter.title}`}>
             {chapter.articles.map(article => (
-              <MenuItem key={`article-${chapter.id}-${article.id}`}>
+              <MenuItem key={`article-${chapter.id}-${article.id}`} onClick={() => handleArticleSelect(chapter.id, article.id)}>
                 {article.title}
               </MenuItem>
             ))}
