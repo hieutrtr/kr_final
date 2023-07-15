87cf7a8d136ef3ac07d2b648f4dedfe30cb5e6e0
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:17:23 +0700

    aider: Set chaptersMock to chapters.
    # Aider chat conversation:
    
    # Context:
    USER: generate chaptersMock set to chapters

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -4,11 +4,28 @@ import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 function SideBar() {
   const [chapters, setChapters] = useState([]);
 
+  const chaptersMock = [
+    {
+      id: 1,
+      articles: [
+        { id: 1, title: 'Article 1' },
+        { id: 2, title: 'Article 2' },
+        { id: 3, title: 'Article 3' }
+      ]
+    },
+    {
+      id: 2,
+      articles: [
+        { id: 4, title: 'Article 4' },
+        { id: 5, title: 'Article 5' },
+        { id: 6, title: 'Article 6' }
+      ]
+    },
+    // ... add more chapters and articles as needed
+  ];
+
   useEffect(() => {
-    fetch('https://api.example.com/chapters')
-      .then(response => response.json())
-      .then(data => setChapters(data))
-      .catch(error => console.error(error));
+    setChapters(chaptersMock);
   }, []);
 
   return (
