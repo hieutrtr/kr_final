b9dfdaa65e4ad45283ac9845a8c08082a8a768a6
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 20:58:06 +0700

    aider: Update the import statement for App in index.js.
    # Aider chat conversation:
    
    # Context:
    USER: Module not found: Can't resolve src/App when npm start

--- workspace/src/index.js
+++ workspace/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/App';
+import App from './App'; // Update the import statement
 
 ReactDOM.render(
   <React.StrictMode>
