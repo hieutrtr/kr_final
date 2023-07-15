759b93ab0e771d4d88486ef4b9478b637a5373ee
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:02:48 +0700

    Removed the sidebar menu and replaced it with an empty div.
    # Aider chat conversation:

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
 import './SideBar.css';
 
 function SideBar() {
@@ -11,17 +9,7 @@ function SideBar() {
   };
 
   return (
-    <aside className={`sidebar ${expanded ? 'expanded' : ''}`}>
-      <div className="sidebar-header" onClick={handleSidebarToggle}>
-        <span className={`arrow ${expanded ? 'expanded' : ''}`}></span>
-        <span>Chapters</span>
-      </div>
-      <Menu className="menu">
-        <MenuItem className="menu-item">Chapter 1</MenuItem>
-        <MenuItem className="menu-item">Chapter 2</MenuItem>
-        <MenuItem className="menu-item">Chapter 3</MenuItem>
-      </Menu>
-    </aside>
+    <div></div>
   );
 }
 
