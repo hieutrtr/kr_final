dfb50555337d0478bcf143ef4dc8dcfca9cae5b0
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 21:14:47 +0700

    Add @szhsin/react-menu package and implement a sidebar menu with chapters.y
    # Aider chat conversation:

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
 
 function SideBar() {
   return (
     <aside className="sidebar">
-      {/* Add your sidebar content here */}
+      <Menu>
+        <SubMenu label="Chapters">
+          <MenuItem>Chapter 1</MenuItem>
+          <MenuItem>Chapter 2</MenuItem>
+          <MenuItem>Chapter 3</MenuItem>
+        </SubMenu>
+      </Menu>
     </aside>
   );
 }
