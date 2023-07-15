b0db43d0e81d180f5d9b37ddfc502d121dbb7868
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 21:19:39 +0700

    aider: Added functionality to expand and collapse the SideBar when the "Chapters" label is clicked.
    # Aider chat conversation:
    
    # Context:
    USER: `SideBar` clickable to expand Chapters

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import './SideBar.css';
 
 function SideBar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <aside className="sidebar">
-      <Menu menuButton={<button>Menu</button>}>
-        <SubMenu label="Chapters">
+      <Menu menuButton={<button onClick={handleExpand}>Menu</button>}>
+        <SubMenu label="Chapters" disabled={!isExpanded}>
           <MenuItem>Chapter 1</MenuItem>
           <MenuItem>Chapter 2</MenuItem>
           <MenuItem>Chapter 3</MenuItem>
