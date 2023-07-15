0a53b010e539e134233392769074baba9fd0d602
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:37:42 +0700

    aider: Generated articles for 60 articles in 12 chapters of OOP.
    # Aider chat conversation:
    
    # Context:
    USER: in `data/ArticleMocks.js` generate `articlesMock` for 60 articles in 12 chapters of OOP

--- data/ArticleMocks.js
+++ data/ArticleMocks.js
@@ -2,49 +2,77 @@ const chaptersMock = [
   {
     id: 1,
     title: 'Chapter 1',
-    articles: [
-      {
-        id: 1,
-        title: 'Article 1'
-      },
-      {
-        id: 2,
-        title: 'Article 2'
-      },
-      {
-        id: 3,
-        title: 'Article 3'
-      }
-    ]
+    articles: []
   },
   {
     id: 2,
     title: 'Chapter 2',
-    articles: [
-      {
-        id: 4,
-        title: 'Article 4'
-      },
-      {
-        id: 5,
-        title: 'Article 5'
-      },
-      {
-        id: 6,
-        title: 'Article 6'
-      }
-    ]
+    articles: []
+  },
+  {
+    id: 3,
+    title: 'Chapter 3',
+    articles: []
+  },
+  {
+    id: 4,
+    title: 'Chapter 4',
+    articles: []
+  },
+  {
+    id: 5,
+    title: 'Chapter 5',
+    articles: []
+  },
+  {
+    id: 6,
+    title: 'Chapter 6',
+    articles: []
+  },
+  {
+    id: 7,
+    title: 'Chapter 7',
+    articles: []
+  },
+  {
+    id: 8,
+    title: 'Chapter 8',
+    articles: []
+  },
+  {
+    id: 9,
+    title: 'Chapter 9',
+    articles: []
+  },
+  {
+    id: 10,
+    title: 'Chapter 10',
+    articles: []
+  },
+  {
+    id: 11,
+    title: 'Chapter 11',
+    articles: []
+  },
+  {
+    id: 12,
+    title: 'Chapter 12',
+    articles: []
   }
 ];
 
 const articlesMock = chaptersMock.flatMap(chapter => {
-  return chapter.articles.map(article => {
-    return {
-      id: article.id,
-      title: article.title,
-      content: `This is the content of ${article.title}`
+  const articles = [];
+  for (let i = 1; i <= 5; i++) {
+    const article = {
+      id: (chapter.id - 1) * 5 + i,
+      title: `Article ${i}`,
+      content: `This is the content of Article ${i}`
     };
-  });
+    chapter.articles.push(article);
+    articles.push(article);
+  }
+  return articles;
 });
 
 export default articlesMock;
