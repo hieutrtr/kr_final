2b06d8e18a70cfb44981fa9c4d58d00aba94f02e
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:15:52 +0700

    aider: Fetch chapters and articles from an API and update the sidebar with the fetched data.
    # Aider chat conversation:
    
    # Context:
    USER: Chapters and Articles should be fetch from an API, mock response of the API

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function SideBar() {
+  const [chapters, setChapters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.example.com/chapters')
+      .then(response => response.json())
+      .then(data => setChapters(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <Sidebar>
       <Menu iconShape="square">
-        {Array.from({ length: 12 }, (_, index) => (
-          <SubMenu key={`chapter-${index + 1}`} label={`Chapter ${index + 1}`}>
-            {Array.from({ length: 3 }, (_, articleIndex) => (
-              <MenuItem key={`article-${index + 1}-${articleIndex + 1}`}>
-                Article {articleIndex + 1}
+        {chapters.map(chapter => (
+          <SubMenu key={`chapter-${chapter.id}`} label={`Chapter ${chapter.id}`}>
+            {chapter.articles.map(article => (
+              <MenuItem key={`article-${chapter.id}-${article.id}`}>
+                {article.title}
               </MenuItem>
             ))}
           </SubMenu>
