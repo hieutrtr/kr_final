b0b8de8467e7355d33643dc019887b1ac155bf15
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 21:57:35 +0700

    aider: Changed `SideBar` items to be displayed as a side bar menu instead of a drop down menu.
    # Aider chat conversation:
    
    # Context:
    USER: change `SideBar` items as side bar menu not drop down menu

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
-import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
+import React from 'react';
+import { Menu, MenuItem } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import './SideBar.css';
 
 function SideBar() {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const handleExpand = () => {
-    setIsExpanded(!isExpanded);
-  };
-
   return (
     <aside className="sidebar">
-      <Menu menuButton={<button onClick={handleExpand}>Menu</button>} open={isExpanded}>
-        <SubMenu label="Chapters">
-          <MenuItem>Chapter 1</MenuItem>
-          <MenuItem>Chapter 2</MenuItem>
-          <MenuItem>Chapter 3</MenuItem>
-        </SubMenu>
+      <Menu>
+        <MenuItem>Chapter 1</MenuItem>
+        <MenuItem>Chapter 2</MenuItem>
+        <MenuItem>Chapter 3</MenuItem>
       </Menu>
     </aside>
   );
