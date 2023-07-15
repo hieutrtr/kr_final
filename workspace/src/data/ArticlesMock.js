66a6c480bd71e5469ae826ef2305e82411aa768c
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:35:17 +0700

    aider: Added articlesMock data to ArticlesMock.js.
    # Aider chat conversation:
    
    # Context:
    USER: in `data/Mocks.js` define const articlesMock to store article data
    USER: in `data/ArticleMocks.js` define const articlesMock to store article data

---
+++ workspace/src/data/ArticlesMock.js
@@ -0,0 +1,19 @@
+const articlesMock = [
+  {
+    id: 1,
+    title: 'Article 1',
+    content: 'This is the content of Article 1'
+  },
+  {
+    id: 2,
+    title: 'Article 2',
+    content: 'This is the content of Article 2'
+  },
+  {
+    id: 3,
+    title: 'Article 3',
+    content: 'This is the content of Article 3'
+  }
+];
+
+export default articlesMock;
