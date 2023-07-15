7296cbc908160e5a6f58e742cc6bcce04c9e5db6
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:00:20 +0700

    aider: Added CSS for menu sidebar to display as a menu sidebar.
    # Aider chat conversation:
    
    # Context:
    USER: change CSS for menu sidebar can display as menu side bar

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import './SideBar.css';
 
 function SideBar() {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleSidebarToggle = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <aside className="sidebar">
-      <Menu menuButton={<button>Chapters</button>}>
-        <MenuItem>Chapter 1</MenuItem>
-        <MenuItem>Chapter 2</MenuItem>
-        <MenuItem>Chapter 3</MenuItem>
+    <aside className={`sidebar ${expanded ? 'expanded' : ''}`}>
+      <div className="sidebar-header" onClick={handleSidebarToggle}>
+        <span className={`arrow ${expanded ? 'expanded' : ''}`}></span>
+        <span>Chapters</span>
+      </div>
+      <Menu className="menu">
+        <MenuItem className="menu-item">Chapter 1</MenuItem>
+        <MenuItem className="menu-item">Chapter 2</MenuItem>
+        <MenuItem className="menu-item">Chapter 3</MenuItem>
       </Menu>
     </aside>
   );
