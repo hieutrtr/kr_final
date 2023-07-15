34791b1a7eac035029dbee4140c5f9821e2438ab
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 21:58:26 +0700

    aider: Added clickable functionality to the SideBar to expand chapters.
    # Aider chat conversation:
    
    # Context:
    USER: `SideBar` clickable to expand Chapters

--- workspace/src/components/SideBar.js
+++ workspace/src/components/SideBar.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Menu, MenuItem, SubMenu } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 
 function SideBar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <aside className="sidebar">
-      <Menu menuButton={<button>Menu</button>}>
-        <SubMenu label="Chapters">
+      <div className="sidebar-header" onClick={handleExpand}>
+        <span>Chapters</span>
+        <span className={`arrow ${isExpanded ? 'expanded' : ''}`}></span>
+      </div>
+      {isExpanded && (
+        <Menu>
           <MenuItem>Chapter 1</MenuItem>
           <MenuItem>Chapter 2</MenuItem>
           <MenuItem>Chapter 3</MenuItem>
-        </SubMenu>
-      </Menu>
+        </Menu>
+      )}
     </aside>
   );
 }
