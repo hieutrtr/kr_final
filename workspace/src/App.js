717038fdbe7d86bd85181cc7d14f01cf2bff3f00
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 23:02:06 +0700

    aider: Refactor App.js to include state variables and a function to handle article selection, and pass the function as a prop to SideBar component and the state variables as props to ContentArea component.
    # Aider chat conversation:
    
    # Context:
    USER: Uncaught TypeError: onArticleSelect is not a function

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopMenu from './components/TopMenu';
 import SideBar from './components/SideBar';
 import ContentArea from './components/ContentArea';
 
 function App() {
+  const [selectedChapterId, setSelectedChapterId] = useState(null);
+  const [selectedArticleId, setSelectedArticleId] = useState(null);
+
+  const handleArticleSelect = (chapterId, articleId) => {
+    setSelectedChapterId(chapterId);
+    setSelectedArticleId(articleId);
+  };
+
   return (
     <div className="app">
       <TopMenu />
       <div className="main-content">
-        <SideBar />
-        <ContentArea />
+        <SideBar onArticleSelect={handleArticleSelect} />
+        <ContentArea selectedChapterId={selectedChapterId} selectedArticleId={selectedArticleId} />
       </div>
     </div>
   );
