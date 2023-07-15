7cf2410cd554dc6c86539bf733d1e0c5c853d2fc
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-15 22:22:47 +0700

    Added additional chapters to the `chaptersMock` array in `Mocks.js`.
    # Aider chat conversation:

--- workspace/src/data/Mocks.js
+++ workspace/src/data/Mocks.js
@@ -1,9 +1,6 @@
-// entire file content ...
-
 const chaptersMock = [
   {
     id: 1,
-    title: 'Introduction to Object Oriented Programming',
     articles: [
       { id: 1, title: 'Introduction to Object Oriented Programming' },
       { id: 2, title: 'Classes and Objects' },
@@ -14,7 +11,6 @@ const chaptersMock = [
   },
   {
     id: 2,
-    title: 'Design Principles',
     articles: [
       { id: 6, title: 'Design Principles' },
       { id: 7, title: 'Object-Oriented Analysis and Design' },
@@ -23,7 +19,106 @@ const chaptersMock = [
       { id: 10, title: 'Object-Oriented Design Patterns' }
     ]
   },
-  // ... rest of the chapters ...
+  {
+    id: 3,
+    articles: [
+      { id: 11, title: 'Object-Oriented Software Engineering' },
+      { id: 12, title: 'Object-Oriented Database Management Systems' },
+      { id: 13, title: 'Object-Oriented Testing' },
+      { id: 14, title: 'Object-Oriented Metrics and Quality' },
+      { id: 15, title: 'Object-Oriented Project Management' }
+    ]
+  },
+  {
+    id: 4,
+    articles: [
+      { id: 16, title: 'Object-Oriented User Interface Design' },
+      { id: 17, title: 'Object-Oriented Web Development' },
+      { id: 18, title: 'Object-Oriented Mobile App Development' },
+      { id: 19, title: 'Object-Oriented Artificial Intelligence' },
+      { id: 20, title: 'Object-Oriented Big Data Analytics' }
+    ]
+  },
+  {
+    id: 5,
+    articles: [
+      { id: 21, title: 'Object-Oriented Cloud Computing' },
+      { id: 22, title: 'Object-Oriented Internet of Things' },
+      { id: 23, title: 'Object-Oriented Cybersecurity' },
+      { id: 24, title: 'Object-Oriented Blockchain Technology' },
+      { id: 25, title: 'Object-Oriented Quantum Computing' }
+    ]
+  },
+  {
+    id: 6,
+    articles: [
+      { id: 26, title: 'Object-Oriented Robotics' },
+      { id: 27, title: 'Object-Oriented Virtual Reality' },
+      { id: 28, title: 'Object-Oriented Augmented Reality' },
+      { id: 29, title: 'Object-Oriented Machine Learning' },
+      { id: 30, title: 'Object-Oriented Data Science' }
+    ]
+  },
+  {
+    id: 7,
+    articles: [
+      { id: 31, title: 'Object-Oriented Natural Language Processing' },
+      { id: 32, title: 'Object-Oriented Computer Vision' },
+      { id: 33, title: 'Object-Oriented Neural Networks' },
+      { id: 34, title: 'Object-Oriented Deep Learning' },
+      { id: 35, title: 'Object-Oriented Reinforcement Learning' }
+    ]
+  },
+  {
+    id: 8,
+    articles: [
+      { id: 36, title: 'Object-Oriented Genetic Algorithms' },
+      { id: 37, title: 'Object-Oriented Evolutionary Computation' },
+      { id: 38, title: 'Object-Oriented Swarm Intelligence' },
+      { id: 39, title: 'Object-Oriented Fuzzy Systems' },
+      { id: 40, title: 'Object-Oriented Expert Systems' }
+    ]
+  },
+  {
+    id: 9,
+    articles: [
+      { id: 41, title: 'Object-Oriented Robotics Process Automation' },
+      { id: 42, title: 'Object-Oriented Internet of Robotic Things' },
+      { id: 43, title: 'Object-Oriented Autonomous Vehicles' },
+      { id: 44, title: 'Object-Oriented Smart Cities' },
+      { id: 45, title: 'Object-Oriented Industry 4.0' }
+    ]
+  },
+  {
+    id: 10,
+    articles: [
+      { id: 46, title: 'Object-Oriented Virtual Assistants' },
+      { id: 47, title: 'Object-Oriented Chatbots' },
+      { id: 48, title: 'Object-Oriented Natural Language Generation' },
+      { id: 49, title: 'Object-Oriented Speech Recognition' },
+      { id: 50, title: 'Object-Oriented Emotion AI' }
+    ]
+  },
+  {
+    id: 11,
+    articles: [
+      { id: 51, title: 'Object-Oriented Computer Graphics' },
+      { id: 52, title: 'Object-Oriented Game Development' },
+      { id: 53, title: 'Object-Oriented Virtual Worlds' },
+      { id: 54, title: 'Object-Oriented Animation' },
+      { id: 55, title: 'Object-Oriented Visual Effects' }
+    ]
+  },
+  {
+    id: 12,
+    articles: [
+      { id: 56, title: 'Object-Oriented Music Technology' },
+      { id: 57, title: 'Object-Oriented Sound Design' },
+      { id: 58, title: 'Object-Oriented Audio Processing' },
+      { id: 59, title: 'Object-Oriented Synthesis' },
+      { id: 60, title: 'Object-Oriented Music Composition' }
+    ]
+  }
 ];
 
 export default chaptersMock;
