5ecad30cc7f099f0fa97a4fc85e6afde3849618b
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-16 17:11:18 +0700

    aider: work in progress
    # Aider chat conversation:
    
    # Context:
    USER: welcome content should present featured article content

--- workspace/src/components/ContentArea.js
+++ workspace/src/components/ContentArea.js
@@ -4,6 +4,7 @@ import articlesMock from '../data/ArticlesMock';
 
 function ContentArea({ selectedChapterId, selectedArticleId, selectedCategory }) {
   const selectedArticle = articlesMock.find(article => article.id === selectedArticleId && (selectedCategory === "" || article.category === selectedCategory));
+  const featuredArticle = articlesMock.find(article => article.featured);
 
   return (
     <main className="content-area">
@@ -14,8 +15,17 @@ function ContentArea({ selectedChapterId, selectedArticleId, selectedCategory })
         </div>
       ) : (
         <div>
-          <h2>Welcome to the Content Area</h2>
-          <p>Select an article from the sidebar to view its content.</p>
+          {featuredArticle ? (
+            <div>
+              <h2>{featuredArticle.title}</h2>
+              <div dangerouslySetInnerHTML={{ __html: featuredArticle.content }}></div>
+            </div>
+          ) : (
+            <div>
+              <h2>Welcome to the Content Area</h2>
+              <p>Select an article from the sidebar to view its content.</p>
+            </div>
+          )}
         </div>
       )}
     </main>
