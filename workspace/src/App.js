30e735e5ef00d9b3e6aceb1b495dec0a4a04cd2c
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-16 16:59:24 +0700

    aider: Added a state variable and useEffect hook to handle the first visit of the website and pass it as a prop to ContentArea component.
    # Aider chat conversation:
    
    # Context:
    USER: when first visit the website should show welcome content

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TopMenu from './components/TopMenu';
 import SideBar from './components/SideBar';
 import ContentArea from './components/ContentArea';
@@ -7,6 +7,11 @@ function App() {
   const [selectedChapterId, setSelectedChapterId] = useState(0);
   const [selectedArticleId, setSelectedArticleId] = useState(0);
   const [selectedCategory, setSelectedCategory] = useState('');
+  const [firstVisit, setFirstVisit] = useState(true);
+
+  useEffect(() => {
+    setFirstVisit(false);
+  }, []);
 
   const handleArticleSelect = (chapterId, articleId) => {
     setSelectedChapterId(chapterId);
@@ -27,7 +32,7 @@ function App() {
           onArticleSelect={(chapterId, articleId) => handleArticleSelect(chapterId, articleId)}
           onCategorySelect={(category) => handleCategorySelect(category)}
         />
-        <ContentArea selectedChapterId={selectedChapterId} selectedArticleId={selectedArticleId} selectedCategory={selectedCategory} />
+        <ContentArea selectedChapterId={selectedChapterId} selectedArticleId={selectedArticleId} selectedCategory={selectedCategory} firstVisit={firstVisit} />
       </div>
     </div>
   );
