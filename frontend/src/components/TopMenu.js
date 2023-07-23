71dfe90a32e29483dce5e0fe4c33d51a0885b4a4
Hieu Tran <hieutrantrung.it@gmail.com>
2023-07-23 14:55:23 +0700

    aider: Added CSS for styling the top menu, separating and highlighting the selected item.
    # Aider chat conversation:
    
    # Context:
    USER: add css for styling top menu, seperate and highligh selected

--- frontend/src/components/TopMenu.js
+++ frontend/src/components/TopMenu.js
@@ -1,21 +1,24 @@
 // entire file content ...
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './TopMenu.css';
 
 function TopMenu({ onSectionTypeSelect }) {
+  const [selectedSection, setSelectedSection] = useState('');
+
   const handleSectionTypeClick = (sectionType) => {
     onSectionTypeSelect(sectionType);
+    setSelectedSection(sectionType);
   };
 
   return (
     <nav className="top-menu">
-      <Link to="/chapter" className="menu-item" onClick={() => handleSectionTypeClick('chapter')}>Lý Thuyết</Link>
-      <Link to="/definition" className="menu-item" onClick={() => handleSectionTypeClick('definition')}>Khái Niệm</Link>
-      <Link to="/concept" className="menu-item" onClick={() => handleSectionTypeClick('concept')}>Tính Chất</Link>
-      <Link to="/exercise" className="menu-item" onClick={() => handleSectionTypeClick('exercise')}>Bài Tập</Link>
-      <Link to="/about" className="menu-item" onClick={() => handleSectionTypeClick('about')}>Thông Tin Nhóm</Link>
+      <Link to="/chapter" className={`menu-item ${selectedSection === 'chapter' ? 'selected' : ''}`} onClick={() => handleSectionTypeClick('chapter')}>Lý Thuyết</Link>
+      <Link to="/definition" className={`menu-item ${selectedSection === 'definition' ? 'selected' : ''}`} onClick={() => handleSectionTypeClick('definition')}>Khái Niệm</Link>
+      <Link to="/concept" className={`menu-item ${selectedSection === 'concept' ? 'selected' : ''}`} onClick={() => handleSectionTypeClick('concept')}>Tính Chất</Link>
+      <Link to="/exercise" className={`menu-item ${selectedSection === 'exercise' ? 'selected' : ''}`} onClick={() => handleSectionTypeClick('exercise')}>Bài Tập</Link>
+      <Link to="/about" className={`menu-item ${selectedSection === 'about' ? 'selected' : ''}`} onClick={() => handleSectionTypeClick('about')}>Thông Tin Nhóm</Link>
     </nav>
   );
 }
